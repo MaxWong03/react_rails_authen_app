a932cfb3eb1acb294e9e912624eeb737c4f0e027
Max Wong <max@techsamurais.com>
2020-01-17 15:47:33 -0800

    edit session_store

--- config/initializers/session_store.rb
+++ config/initializers/session_store.rb
@@ -2,4 +2,9 @@
 
 # key: is the name of the session cookie
 #domain: the domain of the api application (Rails)
-Rails.application.config.session_store :cookie_store, key: "_authentication_app", domain: "jdh-authen-app-api.herokuapp.com"
+
+if Rails.env === "production"
+  Rails.application.config.session_store :cookie_store, key: "_authentication_app", domain: "jdh-authen-app-api.herokuapp.com"
+else
+  Rails.application.config.session_store :cookie_store, key: "_authentication_app"
+end
