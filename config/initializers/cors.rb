e320fec4124d62b4e437b78636ede13315799db2
Max Wong <max@techsamurais.com>
2020-01-17 19:09:43 -0800

    fix registration contorller bug and cors

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -8,7 +8,7 @@
 Rails.application.config.middleware.insert_before 0, Rack::Cors do
   allow do
     #For development
-    origins "http:localhost:3000"
+    origins "http://localhost:3000"
     resource "*", headers: :any, methods:[:get, :post, :put, :patch, :delete, :options, :head],
 credentials: true
   end
